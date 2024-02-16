780c369e79aca01328ecb362fd01dc3bc973ce5e
Mobina.Ghasempour <romi.ghasem@gmail.com>
2024-02-16 12:00:52 +0330

    form element in login file wrote

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,17 @@
 
 const Login = () => {
 
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="username" />
+        <input type="number" placeholder="age"/>
+        <input type="password" placeholder="password" />
+        <input type="password" placeholder="confirm password" />
+        <input type="submit" value='Log in'/>
+      </form>
+
+    </>
+  )
 }
 export default Login;
