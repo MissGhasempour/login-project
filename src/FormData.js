9dd0ea6d868c89a59d2a29f9b47795175916435e
Mobina <98208342+MissGhasempour@users.noreply.github.com>
2025-05-14 14:36:35 +0330

    comment code removed

--- src/FormData.js
+++ src/FormData.js
@@ -1,18 +1,11 @@
 import { useLocation } from "react-router-dom";
 
 const FormData = () => {
-  // let username = document.getElementById("username");
-  // console.log(username);
-  // const searchParams = new URLSearchParams(props.location.search);
-  // const username = searchParams.get('name')
-  // let { username } = props.match.params;
 
   let location = useLocation();
   return (
     <>
       <h1 className="login-information-title">Login Information</h1>
-      {/* {username ? <p>{ username}</p> : <p>not</p>} */}
-      {/* <p>{username }</p> */}
       <div className="form-info">
         <h3>username = {location.state.username}</h3>
         <h3>age = {location.state.age}</h3>
